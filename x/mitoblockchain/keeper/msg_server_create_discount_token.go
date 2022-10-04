7506a9fc415e7142f01b81d79f9eb8bf66c2e721
Daljeet Singh <devdaljeetsingh@gmail.com>
2022-10-04 02:21:39 +0000

    Implement the create discount token msg server

---
+++ x/mitoblockchain/keeper/msg_server_create_discount_token.go
@@ -0,0 +1,32 @@
+package keeper
+
+import (
+	"context"
+
+    "github.com/mitoblock/mitoblockchain/x/mitoblockchain/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+
+func (k msgServer) CreateDiscountToken(goCtx context.Context,  msg *types.MsgCreateDiscountToken) (*types.MsgCreateDiscountTokenResponse, error) {
+	  // Get the context
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+     // Create variable of type DiscountToken
+	 var discountToken = types.DiscountToken{
+        Creator: msg.Creator,
+		Timestamp: msg.timestamp,
+		ActivityName: msg.activityName,
+		Score: msg.score,
+		Message: msg.message,
+		DiscountValue: msg.discountValue,
+		eligibleCompanies: msg.eligibleCompanies
+		ItemType: msg.itemType,
+		ExpiryDate: msg.expiryDate
+    }
+
+	// Add a discount token to the store and get back the ID
+	id := k.AppendDiscountToken(ctx, discountToken)
+
+	return &types.MsgCreateDiscountTokenResponse{Id: id}, nil
+}
