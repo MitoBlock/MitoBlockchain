73e34249c7a7b970835c2af7a9ba58fd0150f35c
Daljeet Singh <devdaljeetsingh@gmail.com>
2022-10-10 01:20:56 +0000

    Create the create membership token method

---
+++ x/mitoblockchain/keeper/msg_server_create_membership_token.go
@@ -0,0 +1,30 @@
+package keeper
+
+import (
+	"context"
+
+    "github.com/mitoblock/mitoblockchain/x/mitoblockchain/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+
+func (k msgServer) CreateMembershipToken(goCtx context.Context,  msg *types.MsgCreateMembershipToken) (*types.MsgCreateMembershipTokenResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+    // Create variable of type Membership token
+    var membershipToken = types.MembershipToken{
+        Creator: msg.Creator,
+        Timestamp: msg.Timestamp,
+		ActivityName: msg.ActivityName, 
+		Score: msg.Score, 
+		Message: msg.Message,
+		MembershipDuration: msg.MembershipDuration,
+		EligibleCompanies: msg.EligibleCompanies,
+		ExpiryDate: msg.ExpiryDate,
+    }
+
+    // Add a Membership token to the store and get back the ID
+    id := k.AppendMembershipToken(ctx, membershipToken)
+
+	return &types.MsgCreateMembershipTokenResponse{Id: id}, nil
+}
